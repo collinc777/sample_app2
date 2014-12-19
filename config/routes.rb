4dacfd3127b5d349e41ee80bdbb97f1bf292367c
Collin <collinc777@aol.com>
2014-12-19 16:31:35 -0600

    Finish layout and routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,9 +1,14 @@
 Rails.application.routes.draw do
+  get 'users/new'
+
   root 'static_pages#home'
+  #want to use the path form for routes as apposed to the url form.
+  #path form below:
 
-  get 'static_pages/help'
-
-  get 'static_pages/about'
+  get 'help'    => 'static_pages#help'
+  get 'about'   => 'static_pages#about'
+  get 'contact' => 'static_pages#contact'
+  get 'signup'  => 'users#new'
 
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
