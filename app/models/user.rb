266fab6309d58c620e53f56bba8a9c381e4d9751
Collin <collinc777@aol.com>
2014-12-26 16:07:29 -0600

    Finish user edit, update, index, and destory actions

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,5 +1,8 @@
+require 'bcrypt'
+
 class User < ActiveRecord::Base
   attr_accessor :remember_token
+
   before_save { self.email = email.downcase }
   validates :name,  presence: true, length: { maximum: 50 }
   VALID_EMAIL_REGEX = /\A[\w+\-.]+@[a-z\d\-.]+\.[a-z]+\z/i
@@ -7,7 +10,7 @@ class User < ActiveRecord::Base
             format: { with: VALID_EMAIL_REGEX },
             uniqueness: { case_sensitive: false }
   has_secure_password
-  validates :password, length: { minimum: 6}
+  validates :password, length: { minimum: 6}, allow_blank: true
 
 
   # Returns the hash digest of the given string.
@@ -22,9 +25,10 @@ class User < ActiveRecord::Base
     SecureRandom.urlsafe_base64
   end
 
+  # Remembers a user in the database for use in persistent sessions.
   def remember
     self.remember_token = User.new_token
-        update_attribute(:remember_digest, User.digest(remember_token))
+    update_attribute(:remember_digest, User.digest(remember_token))
   end
 
   # Returns true if the given token matches the digest.
